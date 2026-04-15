4fcd4260e9910a1135b27fb5f663f02f4f23be15
Fenix-dev-72 <orifjonovsamandar130@gmail.com>
2026-04-15 11:34:18 +0500

    new

---
+++ app/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework.serializers import ModelSerializer
+
+from app.models import Category, Medicine, Supplier, City, Country
+
+
+class CategoryModelSerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id','name')
+
+class MedicineModelSerializer(ModelSerializer):
+    class Meta:
+        model = Medicine
+        fields = ('id','name','category','description','price','stock')
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        request = self.context.get('request')
+
+        if request and request.method == 'GET': # noqa
+            representation.pop('description', None)
+
+        return representation
+
+class MedicineUpdateModelSerializer(ModelSerializer):
+    class Meta:
+        model = Medicine
+        fields = ('id','name','category','description','price','stock')
+
+
+class SupplierModelSerializer(ModelSerializer):
+    class Meta:
+        model = Supplier
+        fields = ('id','name','phone','city',)
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        request = self.context.get('request')
+
+        if request and request.method == 'GET': # noqa
+            representation['city'] = instance.city.name
+
+
+        return representation
+
+
+class CountryModelSerializer(ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ('id','name')
+
+class CityModelSerializer(ModelSerializer):
+    class Meta:
+        model = City
+        fields = ('id','name','country')
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        request = self.context.get('request')
+
+        if request and request.method == 'GET': # noqa
+            representation['country'] = instance.country.name
+
+        return representation
+
---
+++ app/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+
+from app.views import (CategoryListCreateApiView, MedicineListCreateApiView,
+                       MedicineRetrieveUpdateDestroyApiView, SupplierListCreateAPIView,
+                       CountryListAPIView, CityListCreateApiView)
+
+urlpatterns = [
+    path('category',CategoryListCreateApiView.as_view()),
+    path('medicine',MedicineListCreateApiView.as_view()),
+    path('medicine/<int:id>',MedicineRetrieveUpdateDestroyApiView.as_view()),
+    path('supplier',SupplierListCreateAPIView.as_view()),
+    path('country',CountryListAPIView.as_view()),
+    path('city',CityListCreateApiView.as_view()),
+]
